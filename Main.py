bc251e3bd41339981c8f8a01fdad38a15a3c2e97
BOoblover0 <weblogin4333@gmail.com>
2024-07-11 16:27:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,66 @@
+import os
+import logging
+from os import getenv
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import ChatAdminRequired
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+# Edit It From Line 18 to 20
+API_ID = int(os.getenv("27099489"))
+API_HASH = os.getenv("563aa5d8b46526c5ec0252de8f659904")
+BOT_TOKEN = os.getenv("7494130118:AAFszfmsIYgNP6VuQX6OWHbsyuf-8cC9fu8")
+#Dont Edit From Line 21. otherwise you will get error 
+OWNER = os.getenv("@UnknownX_911")
+
+# pyrogram client
+app = Client(
+            "banall",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+)
+
+@app.on_message(
+filters.command("start")
+& filters.private            
+)
+async def start_command(client, message: Message):
+  await message.reply_photo(
+                            photo = f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
+                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
+  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
+                ]       
+           ]
+      )
+)
+
+@app.on_message(
+filters.command("banall") 
+& filters.group
+)
+async def banall_command(client, message: Message):
+    print("getting memebers from {}".format(message.chat.id))
+    async for i in app.get_chat_members(message.chat.id):
+        try:
+            await app.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            print("kicked {} from {}".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("failed to kicked {} from {}".format(i.user.id, e))           
+    print("process completed")
+    
+
+# start bot client
+app.start()
+print("Banall-Bot Booted Successfully")
+idle()
 
